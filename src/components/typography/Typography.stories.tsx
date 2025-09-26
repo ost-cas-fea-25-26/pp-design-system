f5efc5e90771ac5fae7e5adbdc5820a94d41cf54
Marc Oertle <marc@frontify.com>
2025-09-26 11:47:45 +0200

    fix

--- src/components/typography/Typography.stories.tsx
+++ src/components/typography/Typography.stories.tsx
@@ -1,5 +1,5 @@
-import type { Meta, StoryObj } from "@storybook/react";
 import { Typography } from "./Typography";
+import { Meta, StoryObj } from "@storybook/react-vite";
 
 const meta = {
   component: Typography,
