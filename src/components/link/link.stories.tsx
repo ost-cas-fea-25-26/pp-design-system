f908a0b316f37c9240bda0c6b38a904129ad5ba1
Marc Oertle <marc@frontify.com>
2025-10-13 19:32:07 +0200

    improve link component, use Slot

--- src/components/link/link.stories.tsx
+++ src/components/link/link.stories.tsx
@@ -7,7 +7,7 @@ const meta = {
   tags: ["autodocs"],
   parameters: { layout: "centered" },
   argTypes: {
-    label: {
+    title: {
       control: "text",
       description: "Link label",
     },
@@ -28,7 +28,19 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    label: "Click me",
+    asChild: false,
+    children: "Click me",
+    title: "Example Link",
+    target: "_blank",
     href: "https://example.com",
+  },
+};
+
+export const AsChild: Story = {
+  args: {
+    asChild: true,
+    children: <span>Click me!</span>,
+    href: "https://example.com",
+    title: "Button Link",
   },
 };
--- src/components/link/link.tsx
+++ src/components/link/link.tsx
@@ -1,22 +1,34 @@
+import { Slot } from "@radix-ui/react-slot";
+import * as React from "react";
 import { FC } from "react";
 
 type TargetType = "_self" | "_blank";
 
 type LinkProps = {
-  label: string;
+  title: string;
   href: string;
   target?: TargetType;
+  asChild?: boolean;
+  children?: React.ReactNode;
 };
 
-export const Link: FC<LinkProps> = ({ label, href, target = "_self" }) => {
+export const Link: FC<LinkProps> = ({
+  title,
+  href,
+  target = "_self",
+  asChild = false,
+  children,
+}) => {
+  const Component = asChild ? Slot : "a";
+
   return (
-    <a
+    <Component
       href={href}
-      title={label}
+      title={title}
       target={target}
       className="label-sm text-primary hover:decoration-violet-200 hover:underline-offset-4 underline decoration-solid decoration-1 underline-offset-2"
     >
-      {label}
-    </a>
+      {children}
+    </Component>
   );
 };
