da1213d9314a40cb4622f92831ee0a4088d51738
Marc Oertle <marc@frontify.com>
2025-10-24 17:28:39 +0200

    component

---
+++ src/components/avatar-button/avatar-button.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+import { Avatar, AvatarProps } from "../avatar";
+
+type AvatarButtonProps = {
+  avatarProps: AvatarProps;
+  onClick?: () => void;
+};
+
+export const AvatarButton: FC<AvatarButtonProps> = ({
+  avatarProps,
+  onClick,
+}) => {
+  return (
+    <div
+      onClick={onClick}
+      className="p-[8px] cursor-pointer flex justify-center items-center bg-primary-600 hover:bg-primary-700 flex-col rounded-lg"
+    >
+      <Avatar {...avatarProps} size="s" />
+    </div>
+  );
+};
