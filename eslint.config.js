c889c849c8d03dbfc91a3ab5d439aab0635a091e
Marc Oertle <marc@frontify.com>
2025-10-24 09:13:28 +0200

    update storybook to 9.1.13

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,16 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { config as smartiveConfig } from "@smartive/eslint-config";
 
-const config = [
-  ...smartiveConfig("react"),
-  {
-    settings: {
-      react: { version: "detect" },
-    },
-    rules: {
-      "react/forbid-component-props": "off",
-      "import/no-unresolved": "off",
-    },
+const config = [...smartiveConfig("react"), {
+  settings: {
+    react: { version: "detect" },
   },
-];
+  rules: {
+    "react/forbid-component-props": "off",
+    "import/no-unresolved": "off",
+  },
+}, ...storybook.configs["flat/recommended"]];
 
 export default config;
