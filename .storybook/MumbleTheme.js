9b28e484ca68c96dd464c1fa4080bf7733cc458e
Marc Oertle <marc@frontify.com>
2025-10-31 10:47:35 +0100

    create theme

---
+++ .storybook/MumbleTheme.js
@@ -0,0 +1,8 @@
+import { create } from 'storybook/theming';
+
+export default create({
+    base: 'light',
+    brandTitle: 'Mumble UI',
+    brandImage: '/logo/logo.png',
+    brandTarget: '_self',
+});
