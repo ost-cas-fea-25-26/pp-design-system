e4a4dd7dee3444ec42dbd0c5eaca520074b07a5e
Marc Oertle <marc@frontify.com>
2025-10-31 16:33:23 +0100

    feat: logo (#61)
    * add logos
    
    * create theme
    
    * add logo story
    
    * inline rendering
    
    * move logos

---
+++ .storybook/MumbleTheme.js
@@ -0,0 +1,8 @@
+import { create } from 'storybook/theming';
+
+export default create({
+    base: 'light',
+    brandTitle: 'Mumble UI',
+    brandImage: '/logo.png',
+    brandTarget: '_self',
+});
