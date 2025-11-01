fb135f4ef7b0e15afbab3d744edf521cd2cfe861
Raphi <raphael@smartive.ch>
2025-11-01 21:27:03 +0100

    fail if coverage to low

--- vitest.config.js
+++ vitest.config.js
@@ -5,5 +5,19 @@ export default defineConfig({
     globals: true,
     environment: "happy-dom",
     include: ["src/**/*.unit.test.ts?(x)"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+      lines: 80,
+      functions: 80,
+      branches: 80,
+      statements: 80,
+      exclude: [
+        "src/**/*.stories.*",
+        "src/**/*.e2e.test.*",
+        "src/**/index.ts",
+        "src/icons/**/*.svg",
+      ],
+    },
   },
 });
