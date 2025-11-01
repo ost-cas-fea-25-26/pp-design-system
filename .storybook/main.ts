8d9807f7b2b41f513731a6cd1e5185c586b703c0
Raphi <raphael@smartive.ch>
2025-11-01 22:40:03 +0100

    fix build

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,7 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: [
-    "../src/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
+  stories: ["../src/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-docs",
@@ -16,5 +13,13 @@ const config: StorybookConfig = {
     name: "@storybook/react-vite",
     options: {},
   },
+
+  async viteFinal(viteConfig, { configType }) {
+    if (configType === "PRODUCTION") {
+      viteConfig.base = process.env.STORYBOOK_BASE_URL || "/";
+    }
+    return viteConfig;
+  },
 };
+
 export default config;
