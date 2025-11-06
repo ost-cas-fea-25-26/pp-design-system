8d90e9492abdfd7d8fce10c819145be4003cfb03
Raphael Wirth <37732078+rawi96@users.noreply.github.com>
2025-11-06 06:48:16 +0100

    Update src/components/avatar-button/avatar-button.tsx
    Co-authored-by: Marc Oertle <marc@frontify.com>

--- src/components/avatar-button/avatar-button.tsx
+++ src/components/avatar-button/avatar-button.tsx
@@ -13,7 +13,7 @@ export const AvatarButton: FC<AvatarButtonProps> = ({
   <button
     onClick={onClick}
     aria-label={avatarProps.alt}
-    className="p-[8px] cursor-pointer flex justify-center items-center bg-primary-600 hover:bg-primary-700 flex-col rounded-lg"
+    className="p-[8px] cursor-pointer flex justify-center items-center bg-primary-600 hover:bg-primary-700 flex-col rounded-lg focus-ring-neutral"
   >
     <Avatar {...avatarProps} size="s" />
   </button>
