36e96906cab839f3fba59b3e7bc89fce131ec280
Raphael Wirth <37732078+rawi96@users.noreply.github.com>
2025-12-12 17:56:37 +0100

    feat: change avatar url (#160)

--- src/components/avatar/avatar.tsx
+++ src/components/avatar/avatar.tsx
@@ -56,7 +56,7 @@ export const Avatar: FC<AvatarProps> = ({
           size="default"
           isCircular
           onClick={onEditClick}
-          aria-label="Edit Your Avatar Image"
+          aria-label="Edit Your Avatar"
         >
           <EditIcon color="white" />
         </Button>
