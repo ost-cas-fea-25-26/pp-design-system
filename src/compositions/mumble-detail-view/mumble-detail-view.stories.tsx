1b9bed654c5f1e15dab700dd52d11ad2fc3e6b23
Marc Oertle <marc@frontify.com>
2025-12-30 10:32:48 +0100

    update detail view

--- src/compositions/mumble-detail-view/mumble-detail-view.stories.tsx
+++ src/compositions/mumble-detail-view/mumble-detail-view.stories.tsx
@@ -1,7 +1,13 @@
 import { MumbleDetailView } from "@/compositions/mumble-detail-view/mumble-detail-view";
 import { Meta, StoryObj } from "@storybook/react-vite";
 import { MumbleActions, MumbleProps } from "@/compositions";
-import { IconButton, ProfileIcon } from "@/components";
+import {
+  CommentButton,
+  IconButton,
+  ProfileIcon,
+  ReplyFilledIcon,
+  ReplyIcon,
+} from "@/components";
 import { fn } from "storybook/test";
 
 const reply: MumbleProps = {
@@ -15,7 +21,9 @@ const reply: MumbleProps = {
   actions: (
     <MumbleActions
       deepLink="https://mumble.com/mumbles/123456"
-      commentCounter={0}
+      commentButton={
+        <CommentButton label="Comment" icon={<ReplyIcon color="neutral" />} />
+      }
       likeCounter={1}
       liked={true}
     />
@@ -56,7 +64,12 @@ const meta: Meta<typeof MumbleDetailView> = {
       actions: (
         <MumbleActions
           deepLink="https://mumble.com/mumbles/123456"
-          commentCounter={30}
+          commentButton={
+            <CommentButton
+              label="30 Comments"
+              icon={<ReplyFilledIcon color="primary" />}
+            />
+          }
           likeCounter={6}
           liked={false}
         />
